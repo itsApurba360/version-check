176df98593d3d08537db4840926463feb7ae48a5
itsApurba <apurba.apurbaadhikary@gmail.com>
2024-11-12 14:55:19 +0530

    update

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,5 +1,6 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  compatibilityDate: "2024-11-12"
+  compatibilityDate: "2024-11-12",
+  preset: "vercel-static",
 });
