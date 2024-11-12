c633d6d7d10bb1eba4f454b6a527faa7a210e5e0
itsApurba360 <apurba.a@360ithub.com>
2024-11-12 15:39:46 +0530

    Update version.ts

--- server/api/version.ts
+++ server/api/version.ts
@@ -1,3 +1,3 @@
 export default defineEventHandler(() => {
-  return { name: "360IT_HUB!", timestamp: Date.now(), currentVersion: "2", downloadLink: ""};
+  return { name: "360IT_HUB!", timestamp: Date.now(), currentVersion: "2", downloadLink: "https://drive.google.com/file/d/1y2N5dX-hCOILRBPbeQk16wrJut7D5_zx/view?usp=drive_link"};
 });
