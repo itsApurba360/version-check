560e6c355351c068c49ba2b5d62ea969878733fb
itsApurba360 <apurba.a@360ithub.com>
2024-11-13 16:48:28 +0530

    Update version.ts

--- server/api/version.ts
+++ server/api/version.ts
@@ -1,3 +1,3 @@
 export default defineEventHandler(() => {
-  return { name: "LSA", timestamp: Date.now(), currentVersion: "2", downloadLink: "https://drive.google.com/file/d/12Ri80Pv04O8YgtbYZAdkaTCAk6nlUqYw/view?usp=sharing"};
+  return { name: "LSA", timestamp: Date.now(), currentVersion: "3", downloadLink: "https://drive.google.com/file/d/12Ri80Pv04O8YgtbYZAdkaTCAk6nlUqYw/view?usp=drive_link"};
 });
