a4650a1c49365c0a2e45c61902f2a481c4b5a7fc
Apurba Adhikary <apurba.a@360ithub.com>
2025-04-01 16:43:52 +0530

    Update version.ts

--- server/api/version.ts
+++ server/api/version.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(() => {
-  return { name: "LSA", timestamp: Date.now(), currentVersion: "8", downloadLink: "https://play.google.com/store/apps/details?id=com.bizkitthreesixty.lsa", apps:[
+  return { name: "LSA", timestamp: Date.now(), currentVersion: "9", downloadLink: "https://play.google.com/store/apps/details?id=com.bizkitthreesixty.lsa", apps:[
     {name:"LSA",currentVersion: "9", downloadLink: "https://play.google.com/store/apps/details?id=com.bizkitthreesixty.lsa"}
   ]};
 });
